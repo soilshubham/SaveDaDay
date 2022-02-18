0901bb3770f350e47f151ce5695b6bf87831977e
Sahil Shubham <soilshubham@gmail.com>
2022-02-19 02:30:39 +0530

    Added tailwindcss to project

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Birthday Reminder</title>
+        <meta name="description" content="Remember them birthdays..." />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
